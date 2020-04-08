67ce9880e2d4a4e599165f39aad34d2a0a5c49ee
easudderth <erika.sudderth@dot.gov>
2020-04-08 16:01:24 -0700

    Update Index_Outlier_Cleaning.R
    Add method to round predictions to whole number, save files

--- Analysis/Index_Outlier_Cleaning.R
+++ Analysis/Index_Outlier_Cleaning.R
@@ -11,33 +11,32 @@ output.loc = 'Output'
 load(file.path(output.loc, 'Waze_2020_Predicted_Observed_Index.RData'))
 
 # Apply filters to large observed Waze alert outliers----
-plot(Waze2020_indices$count_ACCIDENT) #no clear outliers
+#plot(Waze2020_indices$count_ACCIDENT) #no clear outliers
 summary(Waze2020_indices$count_ACCIDENT) #max ~850
 
-plot(Waze2020_indices$count_JAM) #no clear outliers
+#plot(Waze2020_indices$count_JAM) #no clear outliers
 summary(Waze2020_indices$count_JAM) #max ~8,000
 
-plot(Waze2020_indices$count_WEATHERHAZARD) #some huge outliers
+#plot(Waze2020_indices$count_WEATHERHAZARD) #some huge outliers
 summary(Waze2020_indices$count_WEATHERHAZARD)
 
 # Remove weather/hazard outliers (~6 values from Jan 1 2020 in NY/CT counties - error with data)
 Waze2020_indices$count_WEATHERHAZARD[which(Waze2020_indices$count_WEATHERHAZARD>10000)] <- NA
-plot(Waze2020_indices$count_WEATHERHAZARD)
+#plot(Waze2020_indices$count_WEATHERHAZARD)
 summary(Waze2020_indices$count_WEATHERHAZARD) #range now to ~4800 
 
 # Look at large predicted Waze alert outliers----
-plot(Waze2020_indices$pred_count_ACCIDENT) #some large values - look at source
+#plot(Waze2020_indices$pred_count_ACCIDENT) #some large values - look at source
 Waze2020_indices[which(Waze2020_indices$pred_count_ACCIDENT>600),] #Los Angeles - don't apply filter
 summary(Waze2020_indices$pred_count_ACCIDENT) #Max ~1600
 
-plot(Waze2020_indices$pred_count_JAM) #no clear outliers
+#plot(Waze2020_indices$pred_count_JAM) #no clear outliers
 Waze2020_indices[which(Waze2020_indices$pred_count_JAM>10000),] #Los Angeles - don't apply filter
 summary(Waze2020_indices$pred_count_JAM) #Max ~19,000
 
-plot(Waze2020_indices$pred_count_WEATHERHAZARD) #
+#plot(Waze2020_indices$pred_count_WEATHERHAZARD) #
 Waze2020_indices[which(Waze2020_indices$pred_count_WEATHERHAZARD>3000),] #Los Angeles - don't apply filter
 summary(Waze2020_indices$pred_count_WEATHERHAZARD) #Max ~6700
-
 
 #Explore patterns for predicted values vs historical means----
 plot(Waze2020_indices$pred_count_JAM ~ Waze2020_indices$hist_mean_JAM)
@@ -53,6 +52,7 @@ plot(jam_filt$pred_count_JAM ~ jam_filt$hist_mean_JAM)
 jam_filt2 <- Waze2020_indices[which(Waze2020_indices$pred_count_JAM>10000 & Waze2020_indices$hist_mean_JAM<1),]
 plot(jam_filt$pred_count_JAM ~ jam_filt$hist_mean_JAM)
 #Again, stripe of high predicted values and zero historical is LA, should be fixed with updates
+
 
 #Threshold predicted values to push small numbers to zero----
 #This section should be applied before the impact factors are calculated in the Impact index script
@@ -70,12 +70,25 @@ Waze2020_indices$pred_count_ACCIDENT[pred_ACC_threshold] <- 0
 Waze2020_indices$pred_count_JAM[pred_JAM_threshold] <- 0
 Waze2020_indices$pred_count_WEATHERHAZARD[pred_WEH_threshold] <- 0
 
-#re-calculate impact factors with thresholded values----
+#re-calculate impact factors with thresholded values
 Waze2020_indices <- Waze2020_indices %>%
   mutate(impact_crash_th = (( count_ACCIDENT - pred_count_ACCIDENT) / pred_count_ACCIDENT ),
          impact_weh_th =   (( count_WEATHERHAZARD - pred_count_WEATHERHAZARD) / pred_count_WEATHERHAZARD ),
          impact_jam_th =   (( count_JAM - pred_count_JAM) / pred_count_JAM )
   )
+
+#replace Inf with NA (from dividing by zero)
+Waze2020_indices$impact_crash_th[is.nan(Waze2020_indices$impact_crash_th) == TRUE] <- NA
+Waze2020_indices$impact_weh_th[is.nan(Waze2020_indices$impact_weh_th) == TRUE] <- NA
+Waze2020_indices$impact_jam_th[is.nan(Waze2020_indices$impact_jam_th) == TRUE] <- NA
+
+Waze2020_indices$impact_crash_th[which(Waze2020_indices$impact_crash_th==Inf)] <- NA
+Waze2020_indices$impact_weh_th[which(Waze2020_indices$impact_weh_th==Inf)] <- NA
+Waze2020_indices$impact_jam_th[which(Waze2020_indices$impact_jam_th==Inf)] <- NA
+
+summary(Waze2020_indices$impact_crash_th)
+summary(Waze2020_indices$impact_weh_th)
+summary(Waze2020_indices$impact_jam_th)
 
 # This is the average decrease in activity.
 # Use total instead of average? Or weighted average?
@@ -84,6 +97,46 @@ impact_index_th = ( rowSums(Waze2020_indices[,c('impact_crash_th',
                                             'impact_jam_th')]) / 3 )
 
 Waze2020_indices <- data.frame(Waze2020_indices, impact_index_th)
+
+
+
+#Round predicted values to whole numbers to calculate impact factors----
+#Use this or the threshold option above
+
+#re-calculate impact factors with rounded values----
+Waze2020_indices <- Waze2020_indices %>%
+  mutate(impact_crash_rnd = (( count_ACCIDENT - round(pred_count_ACCIDENT,0)) / round(pred_count_ACCIDENT,0)),
+         impact_weh_rnd =   (( count_WEATHERHAZARD - round(pred_count_WEATHERHAZARD,0)) / round(pred_count_WEATHERHAZARD,0)),
+         impact_jam_rnd =   (( count_JAM - round(pred_count_JAM,0)) / round(pred_count_JAM,))
+  )
+
+#replace NaN with NA
+Waze2020_indices$impact_crash_rnd[is.nan(Waze2020_indices$impact_crash_rnd) == TRUE] <- NA
+Waze2020_indices$impact_weh_rnd[is.nan(Waze2020_indices$impact_weh_rnd) == TRUE] <- NA
+Waze2020_indices$impact_jam_rnd[is.nan(Waze2020_indices$impact_jam_rnd) == TRUE] <- NA
+
+#Replace Inf with NA
+Waze2020_indices$impact_crash_rnd[which(Waze2020_indices$impact_crash_rnd==Inf)] <- NA
+Waze2020_indices$impact_weh_rnd[which(Waze2020_indices$impact_weh_rnd==Inf)] <- NA
+Waze2020_indices$impact_jam_rnd[which(Waze2020_indices$impact_jam_rnd==Inf)] <- NA
+
+summary(Waze2020_indices$impact_crash_th)
+summary(Waze2020_indices$impact_weh_th)
+summary(Waze2020_indices$impact_jam_th)
+
+# This is the average decrease in activity.
+# Use total instead of average? Or weighted average?
+impact_index_rnd = ( rowSums(Waze2020_indices[,c('impact_crash_rnd',
+                                                'impact_weh_rnd',
+                                                'impact_jam_rnd')]) / 3 )
+
+Waze2020_indices <- data.frame(Waze2020_indices, impact_index_rnd)
+
+# Save -----
+save(file = file.path(output.loc, 'Waze_2020_Predicted_Observed_Index.RData'),
+     list = c('Waze2020_indices'))
+
+write.csv(Waze2020_indices, file = file.path(output.loc, 'Waze_2020_Predicted_Observed_Index.csv'), row.names = F)
 
 
 # Histograms by day of week

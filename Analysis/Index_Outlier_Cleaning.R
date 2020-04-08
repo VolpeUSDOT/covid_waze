d7c7e5bb5633b790e991939f254d030d1d9bf907
easudderth <erika.sudderth@dot.gov>
2020-04-07 23:18:59 -0700

    Update Index_Outlier_Cleaning.R

--- Analysis/Index_Outlier_Cleaning.R
+++ Analysis/Index_Outlier_Cleaning.R
@@ -16,7 +16,14 @@ plot(Waze2020_indices$pred_count_JAM ~ Waze2020_indices$hist_mean_JAM)
 #look at high predicted values
 jh <- Waze2020_indices[which(Waze2020_indices$pred_count_JAM>10000),]
 #These are LA - missing historical and 2020 baseline data, but have model results. 
-#Look at plot again when CA data are fixed
+#Look at plot again when CA data are fixed - predicted values are truncated at 5,000, except for the 86 values above 10,000 that are all LA.
+#Explore patterns
+jam_filt <- Waze2020_indices[which(Waze2020_indices$pred_count_JAM<10000),]
+plot(jam_filt$pred_count_JAM ~ jam_filt$hist_mean_JAM)
+
+jam_filt2 <- Waze2020_indices[which(Waze2020_indices$pred_count_JAM>10000 & Waze2020_indices$hist_mean_JAM<1),]
+plot(jam_filt$pred_count_JAM ~ jam_filt$hist_mean_JAM)
+#Again, stipe of high predicted values and zero historical is LA
 
 # Histograms by day of week
 ggplot(Waze2020_indices) + 

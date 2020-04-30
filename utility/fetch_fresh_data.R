e88fad52ce0c7ec5d88e239cb4a4f5a874e4bc36
dflynn-volpe <daniel.flynn@dot.gov>
2020-04-30 12:17:01 -0400

    Adding auth script for auto-export

--- utility/fetch_fresh_data.R
+++ utility/fetch_fresh_data.R
@@ -3,13 +3,19 @@
 auto_export_bucket = 's3://prod-sdc-waze-autoexport-911061262852-us-east-1-bucket/alert/'
 
 
-local_dir = paste0('~/git/covid_waze/Output/', Sys.Date())
+code_loc = '~/git/covid_waze'
+local_dir = file.path(code_loc, 'Output', Sys.Date())
 
 if (!dir.exists(local_dir)) {
   dir.create(local_dir) 
   }
 
-path.expand(local_dir)
+# path.expand(local_dir)
+
+# Refresh credentials
+system(paste0('python ',
+              file.path(path.expand(code_loc), 'utility', 'DF_samlapi_formauth_adfs3_windows.py'))
+       )
 
 system(paste0('aws --profile sdc s3 ls ', auto_export_bucket))
 

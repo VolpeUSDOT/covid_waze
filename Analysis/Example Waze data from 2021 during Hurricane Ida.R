117450dca3d699b532139c13c7f52dad7f465150
Ci (Jessie) Yang <31483478+cyang-volpe@users.noreply.github.com>
2022-07-20 12:17:51 -0400

    Some other exploration on adding basemap

--- Analysis/Example Waze data from 2021 during Hurricane Ida.R
+++ Analysis/Example Waze data from 2021 during Hurricane Ida.R
@@ -6,6 +6,8 @@ library(tidyverse)
 library(maps)
 library(dplyr)
 library(magick)
+library(plotly)
+library(ggmap)
 library(RColorBrewer)
 
 auto_export_bucket = 's3://prod-sdc-waze-autoexport-004118380849/alert/'
@@ -14,19 +16,17 @@ volpe_drive = '//vntscex.local/DFS/Projects/PROJ-OS62A1/SDI Waze Phase 2/Output/
 
 jessie_dr = 'C:/Users/Jessie.Yang.CTR/Downloads/2022 Projects/BTS Waze'
 
-nd <- read_csv(file.path(volpe_drive, 'Waze_2020_National_day.csv'))   
-nw <- read_csv(file.path(volpe_drive, 'Waze_2020_National_week.csv'))
-all <- read_csv(file.path(volpe_drive, 'Waze_Full.csv'))
+#nd <- read_csv(file.path(volpe_drive, 'Waze_2020_National_day.csv'))   
+#nw <- read_csv(file.path(volpe_drive, 'Waze_2020_National_week.csv'))
+#all <- read_csv(file.path(volpe_drive, 'Waze_Full.csv'))
 
 # Visualize the county data
-ct <- all %>% mutate(date= as.Date(date)) %>% ungroup() %>% group_by(date) %>% summarize(total_count = n(), sum_waze_counts = sum(count, na.rm = T))
-gp <- ggplot(ct, aes(x = date, y = sum_waze_counts)) + geom_line() + xlim(Sys.Date() - 60, Sys.Date())
-plotly::ggplotly(gp)
+#ct <- all %>% mutate(date= as.Date(date)) %>% ungroup() %>% group_by(date) %>% summarize(total_count = n(), sum_waze_counts = sum(count, na.rm = T))
+#gp <- ggplot(ct, aes(x = date, y = sum_waze_counts)) + geom_line() + xlim(Sys.Date() - 60, Sys.Date())
+#plotly::ggplotly(gp)
 
 
 # by county, date
-library(tidyverse)
-library(plotly)
 
 maps::county.fips %>%
   as.tibble %>% 
@@ -46,36 +46,43 @@ dall %>%
 la <- read.csv(file.path(jessie_dr, "LA_hurricane_export","LA_hurricane_processed.csv"))
 lasub <- la %>% mutate (fips = paste0(sprintf("%02d", STATEFP), sprintf("%03d", COUNTYFP))) %>% 
   mutate(date= as.Date(yearday), fips = as.numeric(fips)) %>% 
-  filter(date <= '2021-08-31' & date >= '2021-08-15') %>% 
-  group_by(date, fips,sub_type) %>% 
+  #filter(date <= '2021-08-31' & date >= '2021-08-15') %>% 
+  group_by(STATEFP, date, fips,sub_type) %>% 
   summarize(total_count = n(), sum_waze_counts = sum(count, na.rm = T))  
 
-dt <- dall%>% left_join(lasub) %>% mutate()
+dt <- dall%>% left_join(lasub) # the final joined data table for plotting purpose
 
-
+# plot for a single day
+dt_la <- dt %>%  filter(STATEFP == 22)
 
 gp <- dt %>% 
-  filter(date == '2021-08-31')  %>% 
+  filter(STATEFP == 22 & date == '2021-08-31' & sub_type == "HAZARD_WEATHER_FLOOD")  %>% 
   ggplot(aes(long, lat, group = group, text = paste(date,":",sub_type))) +
   geom_polygon(aes(fill = sum_waze_counts), color = "gray70") +
   #geom_polygon(data = dall, color = "gray70") +
-  coord_map() + scale_colour_manual(values = rev(brewer.pal(3, "BuPu"))) + 
-  facet_wrap(~ sub_type) + ggtitle('2021-08-31')
+  coord_map() + scale_colour_manual(values= c( "1" = "#EDF8FB", "2"= "#BFD3E6", "3"= "#9EBCDA",
+                                                  "4"= "#8C96C6", "5"= "8C6BB1", "6"= "#88419D","7"= "#6E016B"), 
+                                    breaks = c("1", "2", "3", "4","5","6")) + 
+  facet_wrap(~ sub_type) + ggtitle('2021-08-31') +
+  ylim(min(dt_la$lat), max(dt_la$lat)) +
+  xlim(min(dt_la$long), max(dt_la$long))
   
 gp
 ggplotly(gp)
 
-
 unique(la$sub_type) # 25 sub-types.
 
-# Create a gif file
+
+
+
+# Create a gif file for multiple days
 for (y in as.character(unique(dt$date[!is.na(dt$date)]))) {
   
   gp <- dt %>% 
     filter(date == y)  %>% 
     ggplot(aes(long, lat, group = group, text = paste(date,":",sub_type))) +
     geom_polygon(aes(fill = sum_waze_counts), color = "gray70") +
-    coord_map() + scale_colour_manual(values = rev(brewer.pal(3, "BuPu"))) + 
+    coord_map() + scale_colour_manual(values = rev(brewer.pal(7, "BuPu"))) + 
     facet_wrap(~ sub_type) + ggtitle(paste0("All type ", y))
   
   fp <- file.path(jessie_dr, paste0("All type", y, ".png"))
@@ -95,6 +102,10 @@ list.files(path=jessie_dr, pattern = '^All type', full.names = TRUE) %>%
 
 
 # make a gif with only one sub-type
+max(dt$sum_waze_counts[dt$sub_type == 'HAZARD_WEATHER_FLOOD'], na.rm = T) # max = 7
+unique(dt$sum_waze_counts[dt$sub_type == 'HAZARD_WEATHER_FLOOD' & !is.na(dt$sum_waze_counts)])
+myPal <- brewer.pal(7, "BuPu")
+
 for (y in as.character(unique(dt$date[!is.na(dt$date)]))) {
   subtype = "HAZARD_WEATHER_FLOOD"
   
@@ -104,9 +115,12 @@ for (y in as.character(unique(dt$date[!is.na(dt$date)]))) {
       filter(date == y & sub_type == subtype)  %>% 
       ggplot(aes(long, lat, group = group, text = paste(date,":",sub_type))) +
       geom_polygon(aes(fill = sum_waze_counts), color = "gray70") +
-      coord_map() + scale_colour_manual(values = rev(brewer.pal(3, "BuPu"))) + 
+      coord_map() + scale_colour_manual(values = c( "1" = "#EDF8FB", "2"= "#BFD3E6", "3"= "#9EBCDA",
+                                                    "4"= "#8C96C6", "5"= "8C6BB1", "6"= "#88419D","7"= "#6E016B"), 
+                                        breaks = c("1", "2", "3", "4","5","6")) + 
       #facet_wrap(~ sub_type) + 
-      ggtitle(paste0(subtype," ", y))
+      ggtitle(paste0(subtype," ", y)) + ylim(min(dt_la$lat), max(dt_la$lat)) +
+      xlim(min(dt_la$long), max(dt_la$long))
     
     fp <- file.path(jessie_dr, paste0(subtype," ", y, ".png"))
     
@@ -126,6 +140,9 @@ list.files(path=jessie_dr, pattern = '^HAZARD_WEATHER_FLOOD', full.names = TRUE)
 
 
 
+max(dt$sum_waze_counts)
+
+
 
 ############ tests ###########################
 # Plot in a map
@@ -143,5 +160,60 @@ counties <- county.fips %>% left_join(df_pop_county, by=c('fips'='region'))
 map("county", fill=TRUE, col=counties$color)
 
 
+############## add a base map ###################
+#la_bm <- get_map(location = c(lon = -92.329102, lat = 30.391830), zoom = 11,  maptype = 'terrain-background', source = 'stamen')
 
+#gp + ggmap(la_bm) 
+#+ coord_sf(crs = st_crs(4326))
+
+library(basemaps)
+get_maptypes()
+set_defaults(map_service = "osm_stamen", map_type = "toner")
+
+data(ext)
+basemap_magick(ext, map_service = "osm", map_type = "topographic")
+basemap_magick(ext, map_service = "osm_stamen", map_type = "toner")
+
+ext_la <- draw_ext()
+basemap_magick(ext_la, map_service = "osm_stamen", map_type = "toner")
+
+basemap_plot(ext)
+basemap_ggplot(ext)
+
+bm <- ggplot() + basemap_gglayer(ext_la) + 
+  coord_sf() +
+  scale_fill_identity()
+bm
+
+ggplot() + geom_polygon(data = dt %>% 
+                          filter(STATEFP == 22 & date == '2021-08-31'), 
+                        aes(long, lat, group = group)) + 
+  basemap_gglayer(ext_la) + 
+  coord_sf() +
+  scale_fill_identity()
+
+
+
+######################### No need ##############
+require(rgdal)
+library(sf)
+state <- readOGR(dsn = file.path(jessie_dr,'state'), layer = "s_22mr22")
+county <- readOGR(dsn = file.path(jessie_dr,'county'), layer = "c_07se22")
+data.frame(state)
+data.frame(county)
+la_sh <- subset(state, STATE == 'LA')
+la_ct <- subset(county, STATE == 'LA')
+plot(la_sh)
+plot(la_ct)
+data.frame(la_ct)
+
+ggplot() + geom_polygon(data = fortify(la_sh), aes(long, lat, group = group), 
+                        colour = alpha("darkred", 1/2), size = 0.7, fill = 'skyblue', alpha = .3)
+ggplot() + geom_polygon(data = fortify(la_ct), aes(long, lat, group = group), 
+                        colour = alpha("darkred", 1/2), size = 0.7, fill = 'skyblue', alpha = .3)
+
+
+ggplot() + geom_sf(data = la_sh, fill = "grey", color = "grey", geometry = geometry) 
+
+ggplot() +geom_sf(data = la_ct)
 
